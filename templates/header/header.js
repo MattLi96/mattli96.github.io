b7a39ef6de725e7cf56f99f83adb60461d549193
Matthew Li <ml927@cornell.edu>
2017-01-06 09:41:29 -0500

    adjust to template url

--- templates/header/header.js
+++ templates/header/header.js
@@ -2,6 +2,6 @@ var app = angular.module("myWebsite", []);
 app.directive("myHeader", function () {
   return {
     restrict: "E",
-    template: "templates/header/header.html"
+    templateUrl: "templates/header/header.html"
   };
 });
