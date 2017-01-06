918c27bf9eaa4afb655975141cfb636b627479fc
Matthew Li <ml927@cornell.edu>
2017-01-06 10:49:30 -0500

    another fix

--- templates/header/header.js
+++ templates/header/header.js
@@ -1,5 +1,4 @@
-var app = angular.module("myWebsite", []);
-app.directive("myHeader", function () {
+angular.module("myWebsite").directive("myHeader", function () {
   return {
     restrict: "E",
     templateUrl: "templates/header/header.html"
